941110698b6353daaa8b3b193fa2e6d19bcf3aa7
Jose-Luis-Nunez <josenunez@outlook.de>
2021-11-20 23:47:35 +0100

    update test for check24

---
+++ src/test/kotlin/pageobject/check24/Check24ResultPage.kt
@@ -0,0 +1,15 @@
+package pageobject.check24
+
+import org.fluentlenium.core.domain.FluentWebElement
+import org.openqa.selenium.support.FindBy
+import pageobject.AbstractScreen
+
+class Check24ResultPage : AbstractScreen() {
+    @FindBy(css = ".HeaderResult__orange")
+    private lateinit var header: FluentWebElement
+
+    fun getHeader(): FluentWebElement {
+        waitUntilElementDisplayed(header)
+        return header
+    }
+}
--- src/test/kotlin/tests/check24/Check24Test.kt
+++ src/test/kotlin/tests/check24/Check24Test.kt
@@ -2,8 +2,10 @@ package tests.check24
 
 import config.annotations.Browser
 import config.driver.Breakpoint
+import org.fluentlenium.assertj.FluentLeniumAssertions.assertThat
 import org.fluentlenium.core.annotation.Page
 import org.junit.jupiter.api.Test
+import pageobject.check24.Check24ResultPage
 import pageobject.check24.Check24StartPage
 import tests.SeleniumTest
 
@@ -13,6 +15,9 @@ class Check24Test : SeleniumTest() {
     @Page
     lateinit var startPage: Check24StartPage
 
+    @Page
+    lateinit var resultPage: Check24ResultPage
+
     @Test
     fun `test search for product`() {
         startPage {
@@ -20,5 +25,6 @@ class Check24Test : SeleniumTest() {
             acceptCookies()
             searchForProduct("PS5")
         }
+        assertThat(resultPage.getHeader()).isDisplayed
     }
 }
